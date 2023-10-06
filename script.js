20639b8d61e804cad50d4c2092fcd92dd5024596
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2023-10-05 23:56:48 -0300

    Comentários

--- script.js
+++ script.js
@@ -13,20 +13,18 @@ for(let i=1; i<nav.length; i++){
         body.classList.add("corpo")
         aux=false
     })
-
     nav[2].addEventListener("click", function(){
         modalProjetos.style.display="block"
         body.classList.add("corpo")
         aux=false
     })
 
-
     // Sublinhar o Nav
     nav[i].addEventListener("mouseover", function(){
         nav[i].style.borderBottom="1px solid black"
     })
     nav[i].addEventListener("mouseout", function(){
-        if(aux==true){
+        if(aux){
             nav[i].style.borderBottom="none"
         }
     })
@@ -41,10 +39,8 @@ for(let i=1; i<nav.length; i++){
 }
 
 
-// Jornada
-// Animação de Digitação do texto
-const p1=document.querySelector(".texto1");
-const p2=document.querySelector(".texto2");
+const p1=document.querySelector(".p1");
+const p2=document.querySelector(".p2");
 const texto1="Sou uma designer de interfaces de usuário (UI) com uma paixão profunda por criar experiências digitais envolventes e esteticamente agradáveis. Minha jornada no mundo do design começou quando cursei um curso técnico de informática no IFSC, onde fui introduzida ao vasto e fascinante universo da computação. Durante esse período, minha curiosidade pelo design de interfaces foi despertada.";
 const texto2="Essa paixão me levou a buscar conhecimento e aprimoramento, e então decidi cursar Ciência da Computação na renomada Universidade Federal de Santa Catarina (UFSC). Durante minha graduação, mergulhei fundo nos aspectos teóricos e práticos da computação, o que me proporcionou uma compreensão sólida dos fundamentos tecnológicos.";
 const intervaloJornada=40; 
@@ -57,16 +53,16 @@ nav[1].addEventListener("click", function(){
 })
 
 function animacaoTexto1(p1, texto1, intervaloJornada){
-    const letra1=texto1.split("").reverse();
+    const letra1=texto1.split("").reverse(); //o split pega cada letra do texto e faz um array, o reverse serve para corrigir o pop posteriormente
     const digitador1=setInterval(()=>{
 
-    if(!letra1.length){
-        clearInterval(digitador1);
+    if(!letra1.length){ // verificar se a array letra1 está vazia
+        clearInterval(digitador1); //termina a função
         animacaoTexto2(p2, texto2, intervaloJornada);
         return;
     }
 
-    const proximo1=letra1.pop();
+    const proximo1=letra1.pop(); // pop remove o último elemento de um array e retorna aquele valor
     p1.innerHTML+=proximo1;
 
 }, intervaloJornada)
@@ -90,8 +86,6 @@ function animacaoTexto2(p2, texto2, intervaloJornada){
 
 }
 
-// Modal dos Projetos
-// Aleatorizar Números
 let intervaloProjetos2=5000;
 let intervaloProjetos3=5500;
 let min=1;
@@ -104,13 +98,14 @@ nav[2].addEventListener("click", function(){
     h3.innerHTML="Selecionando Projeto...";
     for(var i=min; i<=max; i++){
         setTimeout(function(){
-        let sorteio= Math.random()*5;
-        sorteio=Math.ceil(sorteio);
+            let sorteio= Math.random()*5;
+            sorteio=Math.ceil(sorteio);
 
-        textoProjetos.innerHTML=sorteio;
+            textoProjetos.innerHTML=sorteio;
 
-    }, i*(intervaloProjetos1+=5))
-}
+        }, i*(intervaloProjetos1+=5))
+    }
+
     setTimeout(function(){
         h3.innerHTML="Projeto Selecionado!";
     }, intervaloProjetos2)
@@ -123,9 +118,9 @@ nav[2].addEventListener("click", function(){
         }else if(textoProjetos.innerHTML==3){
             window.open('projetos/trabalhodeartes');
         }else if(textoProjetos.innerHTML==4){
-            window.open('projetos/parisatabriz') //não está abrindo
+            window.open('projetos/parisatabriz') 
         }else if(textoProjetos.innerHTML==5){
-            window.open('projetos/cadastropetshop/petshop.html') // não está abrindo
+            window.open('projetos/cadastropetshop/petshop.html') 
         }
         modalProjetos.style.display="none"
         body.classList.remove("corpo")
